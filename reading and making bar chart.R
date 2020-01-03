bcff46877247d8d54ae95a295e7acc11bb8b641c
Haddad <1411058014@mil>
2020-01-03 13:34:31 -0500

    basic r thing

---
+++ reading and making bar chart.R
@@ -0,0 +1,13 @@
+#replace the filepath below with where your data lives. remember the double \\s
+MyData <- read.csv(file="C:\\Users\\HaddadAE\\Git Repos\\exploring r\\data\\FiveFac.csv", header=TRUE, sep=",")
+print(class(MyData)) #to confirm it's a data frame: 
+#this is a good resource for reading about data frames, which is what your data is
+#https://cran.r-project.org/web/packages/data.table/vignettes/datatable-intro.html
+#here are your column names
+print(colnames(MyData))
+#this gives you counts by a variable and puts them in a new variable called counts
+counts <- table(MyData$F_SEX_FINAL)
+print(counts)
+#here's some documentation on counts and bar charts
+barplot(counts, main="People By Sex",
+        xlab="Men and Women")
